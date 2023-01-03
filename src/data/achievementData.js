4e07d9241634ac2fd6aa588c567fa62224469aaa
YoussefAkuma <businessallthetime77@gmail.com>
2023-01-03 20:14:40 +0100

    update pdf cv

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -17,22 +17,6 @@ export const achievementData = {
             field : '',
             image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Cisco_logo_blue_2016.svg/1200px-Cisco_logo_blue_2016.svg.png'
         },
-        {
-            id : 3,
-            title : 'CCNA3(en cours)',
-            details : 'Cisco Certified Network Associate N3.',
-            date : '30-06-2023',
-            field : '',
-            image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Cisco_logo_blue_2016.svg/1200px-Cisco_logo_blue_2016.svg.png'
-        },
-        {
-            id : 4,
-            title : 'Cyber Ops(en cours)',
-            details : '',
-            date : '30-06-2023',
-            field : '',
-            image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_zi-9yIoBDMNiH43qtmayKmcszA-ZbT6v0i_qwa5Ydoix132VnPPuVPBUtbqp3gUd06A&usqp=CAU'
-        }
 
     ]
 }
